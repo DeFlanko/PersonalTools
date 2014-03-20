ec0397cad72902c9adb83c28b7d5bf6c3353ed4c
DeFlanko <DeFlanko@users.noreply.github.com>
2014-03-20 16:04:50 -0700

    Create User_Load.ps1
    Using the CSV file it will launch an application from Citrix to test Server load.

---
+++ Powershell/Citrix/User_Load.ps1
@@ -0,0 +1,24 @@
+[System.Reflection.Assembly]::LoadFile("C:\Program Files (x86)\Citrix\ICA Client\WfIcaLib.dll") 
+
+function CPSLogin1 ($UserName,$Password,$ServerName,$Application)
+{
+    $ICA = New-Object WFICALib.ICAClientClass 
+    $ICA.Address = $ServerName
+    $ICA.Username = $UserName
+    $ICA.InitialProgram = "#$Application"
+    $ICA.SetProp("Password",$Password) 
+    $ICA.Domain = "mmc"
+    $ICA.Launch = $true
+    $ICA.Connect()
+}
+$userlist = Import-Csv .\file.csv
+foreach ($user in $userlist)
+{
+    #Below are the tables form the CSV
+    CPSLogin1 -UserName $user.username -Password $user.password -ServerName $user.ServerName -Application $user.Application
+    #$user.UserName
+    #$user.Password
+    #$user.ServerName
+    $user.Application
+    Start-Sleep -Seconds 4
+}
