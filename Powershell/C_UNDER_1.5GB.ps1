4a92f592add285963349c04de7e4ec3747a2f3da
DeFlanko <DeFlanko@users.noreply.github.com>
2014-03-28 12:42:18 -0700

    Update C_UNDER_1.5GB.ps1
    code cleanup.

--- Powershell/C_UNDER_1.5GB.ps1
+++ Powershell/C_UNDER_1.5GB.ps1
@@ -4,6 +4,9 @@ $ErrorActionPreference = "SilentlyContinue"
 function RunCleanup ($strComputer)
 {
 
+# Static Locations
+    $strFileRepo = "mhcalbsysadpv01\SysAdmins\Scripts\Powershell_Scripts\Clean_C_Drive"
+
 # Add in a Time Stamp
 Write-Host "Start Time"
 (get-date).toString(‘HH:mm:ss MM-dd-yyyy’)
@@ -42,7 +45,7 @@ Write-Host -foregroundcolor Green "=======================================BEFORE
 "$strComputer C: has {0:#.0} GB free of {1:#.0} GB Total" -f ($disk.FreeSpace/1GB),($disk.Size/1GB) | Write-Output
 Write-Host -foregroundcolor Green "================================================================================================="
 
-Write-Host -foregroundcolor Yellow "================================================================================================="
+Write-Host -foregroundcolor Yellow "=======================================FILES TO BE CLEANED======================================"
 Write-Host -foregroundcolor Yellow "Now Identifying Sizes of Folders in $strComputer"
 
 # We identify the folders we want to Measure and their contents.
@@ -88,44 +91,63 @@ $report = @(
         Sum = ($_.Group | Measure-Object Average -Sum).Sum
      }
  }
-Write-Host -foregroundcolor Yellow "=======================================FILES TO BE CLEANED======================================"
-
-Remove-Item $strFolder1\* -Recurse -Force -WhatIf
+Write-Host -foregroundcolor Yellow "Files to be removed on $strComputer"
+# Identifying the Files to be deleted by using the "-WhatIf"
 Remove-Item $strFolder2\* -Recurse -Force -WhatIf
 Remove-Item $strFolder3\* -Recurse -Force -WhatIf
 Remove-Item $strFolder4\* -Recurse -Force -WhatIf
 Remove-Item $strFolder5\* -Recurse -Force -WhatIf
+Get-ChildItem $strFolder1 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_7 } | Remove-Item -Recurse -Force -WhatIf
 Get-ChildItem $strFolder6 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_7 } | Remove-Item -Recurse -Force -WhatIf
 Get-ChildItem $strFolder7 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_270 } | Remove-Item -Recurse -Force -WhatIf
 Get-ChildItem $strFolder9 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_365 } | Remove-Item -Recurse -Force -WhatIf
 
-# Now we delete the contents of the folders. To test the folder add "-whatif" at the end.
+Write-Host -foregroundcolor Yellow "================================================================================================"
+Write-Host -foregroundcolor Cyan "================================================================================================"
+# Using the same script as above, delete the contents of the folders.
+ 
 Write-Host -foregroundcolor Cyan "Now Cleaning Folders in $strComputer" 
 
-Remove-Item $strFolder1\* -Recurse -Force
 Remove-Item $strFolder2\* -Recurse -Force
 Remove-Item $strFolder3\* -Recurse -Force
 Remove-Item $strFolder4\* -Recurse -Force
 Remove-Item $strFolder5\* -Recurse -Force
+Get-ChildItem $strFolder1 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_7 } | Remove-Item -Recurse -Force
 Get-ChildItem $strFolder6 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_7 } | Remove-Item -Recurse -Force
 Get-ChildItem $strFolder7 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_270 } | Remove-Item -Recurse -Force
 Get-ChildItem $strFolder9 -Recurse | Where-Object { $_.LastWriteTime -lt $del_date_365 } | Remove-Item -Recurse -Force
 
-# Attempt to Empty the Recyclebin on the Remote Machine
-Write-Host -foregroundcolor Cyan "Now Cleaning Recyclebin on $strComputer" 
-Invoke-Command -ScriptBlock {c:\scripts\psexec.exe -accepteula \\$strComputer cmd.exe /c del /q /s /f c:\`$recycle.bin} 
+### PROFILE CLEAN UP ###
+# Copy DelProf_2k8 to $strComputer
+Write-Host -foregroundcolor Cyan "Now Copying DelProf_2k8 to $strComputer"
+Copy-Item "\\$strFileRepo\DelProf_2k8.exe" "\\$strComputer\C$\Scripts\"
 
 # Run DelProf_2k8
 Write-Host -foregroundcolor Cyan "Now Running DelProf_2k8 on $strComputer"
 Invoke-Command -ScriptBlock {c:\scripts\psexec.exe -accepteula \\$strComputer C:\scripts\delprof_2k8.exe}
 
+### SEP CLEAN UP ###
+# Copy RemoveStaleVirusDefs.exe to $strComputer
+Write-Host -foregroundcolor Cyan "Now Copying RemoveStaleVirusDefs.exe to $strComputer"
+Copy-Item "\\$strFileRepo\RemoveStaleVirusDefs.exe" "\\$strComputer\C$\Scripts\"
+
+# Run RemoveStaleVirusDefs.exe
+Write-Host -foregroundcolor Cyan "Now Running RemoveStaleVirusDefs.exe on $strComputer"
+Invoke-Command -ScriptBlock {c:\scripts\psexec.exe -accepteula \\$strComputer C:\scripts\RemoveStaleVirusDefs.exe}
+
+# Attempt to Empty the Recyclebin on the Remote Machine
+Write-Host -foregroundcolor Cyan "Now Cleaning Recyclebin on $strComputer" 
+Invoke-Command -ScriptBlock {c:\scripts\psexec.exe -accepteula \\$strComputer cmd.exe /c del /q /s /f c:\`$recycle.bin} 
+
+Write-Host -foregroundcolor Cyan "================================================================================================"
+
 # Calculate the Free Space on "C"
 $size = ([wmi]"\\$strComputer\root\cimv2:Win32_logicalDisk.DeviceID='c:'").Size
 $free = ([wmi]"\\$StrComputer\root\cimv2:Win32_logicalDisk.DeviceID='c:'").FreeSpace
 $disk = ([wmi]"\\$strComputer\root\cimv2:Win32_logicalDisk.DeviceID='c:'")
-Write-Host -foregroundcolor Green "========================================AFTER CLEAN UP==========================================="
+Write-Host -foregroundcolor Green "========================================AFTER CLEAN UP=========================================="
 "$strComputer C: has {0:#.0} GB free of {1:#.0} GB Total" -f ($disk.FreeSpace/1GB),($disk.Size/1GB) | Write-Output
-Write-Host -foregroundcolor Green "================================================================================================="
+Write-Host -foregroundcolor Green "================================================================================================"
 
 # Add in a Time Stamp
 Write-Host "End Time"
@@ -136,7 +158,7 @@ Write-Host "End Time"
 function GetInput ($DefaultText = "",$LabelMessage = "Please enter the information in the space below:",$MultiLine = $true)
 {
     $objForm = New-Object System.Windows.Forms.Form 
-    $objForm.Text = "Data Entry Form"
+    $objForm.Text = "Server List Input Box"
     $objForm.Size = New-Object System.Drawing.Size(300,200) 
     $objForm.StartPosition = "CenterScreen"
 
@@ -148,14 +170,14 @@ function GetInput ($DefaultText = "",$LabelMessage = "Please enter the informati
     $objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") {$objForm.Close()}})
 
     $OKButton = New-Object System.Windows.Forms.Button
-    $OKButton.Location = New-Object System.Drawing.Size(75,120)
+    $OKButton.Location = New-Object System.Drawing.Size(75,125)
     $OKButton.Size = New-Object System.Drawing.Size(75,23)
     $OKButton.Text = "OK"
     $OKButton.Add_Click({$objForm.Close()})
     $objForm.Controls.Add($OKButton)
 
     $CancelButton = New-Object System.Windows.Forms.Button
-    $CancelButton.Location = New-Object System.Drawing.Size(150,120)
+    $CancelButton.Location = New-Object System.Drawing.Size(150,125)
     $CancelButton.Size = New-Object System.Drawing.Size(75,23)
     $CancelButton.Text = "Cancel"
     $CancelButton.Add_Click({$objForm.Close()})
@@ -163,13 +185,13 @@ function GetInput ($DefaultText = "",$LabelMessage = "Please enter the informati
 
     $objLabel = New-Object System.Windows.Forms.Label
     $objLabel.Location = New-Object System.Drawing.Size(10,10) 
-    $objLabel.Size = New-Object System.Drawing.Size(280,40) 
+    $objLabel.Size = New-Object System.Drawing.Size(280,20) 
     $objLabel.Text = $LabelMessage
     $objForm.Controls.Add($objLabel) 
 
     $objTextBox = New-Object System.Windows.Forms.TextBox 
-    $objTextBox.Location = New-Object System.Drawing.Size(10,60) 
-    $objTextBox.Size = New-Object System.Drawing.Size(260,60)
+    $objTextBox.Location = New-Object System.Drawing.Size(10,30) 
+    $objTextBox.Size = New-Object System.Drawing.Size(260,85)
     If($MultiLine)
     {
         $objTextBox.multiline = $true
@@ -188,7 +210,7 @@ function GetInput ($DefaultText = "",$LabelMessage = "Please enter the informati
     $objTextBox.Text
 } 
 
-$ItemList = GetInput -LabelMessage "Input servers to process" -MultiLine $true
+$ItemList = GetInput -LabelMessage "Input FQDN of Servers to Process:" -MultiLine $true
 $ItemList = $ItemList.Split()
 
 foreach ($Item in $ItemList)
