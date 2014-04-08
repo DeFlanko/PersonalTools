ecbaff17340932c29499b5d11376ba58cf979f8e
DeFlanko <DeFlanko@users.noreply.github.com>
2014-04-08 09:04:24 -0700

    Update Get-PageFile_and_Disk_Report.ps1
    Convert to a Function and argument txt file lookup.

--- Powershell/Get-PageFile_and_Disk_Report.ps1
+++ Powershell/Get-PageFile_and_Disk_Report.ps1
@@ -1,6 +1,25 @@
-$Results = @()
+<#  .Description
+    A Function to look to a List of Servers in a TXT file to return Page File Values and Hard Disk values.
+    
+    .Example
+    Load Server names in TXT file specified then Run:
+    Get-PF_and_Disk_Report "Selected Txt File"
+    Output would be something like:
+    
+    Server          OS Version         PageFileLocation PageFileSize PhysicalMemory PF to Ram % Size of C - GB Free on C - GB % Free on C
+    ------          ----------         ---------------- ------------ -------------- ----------- -------------- -------------- -----------
+    SERVER1         Server 2003 SP2    c:\pagefile.sys  5000         8191           61.0400 %   39.998         27.223         68.0600 %  
+    SERVER2         Server 2008 R2 SP1 System Managed   1024         1023           0.0000 %    31.9           6.246          19.5800 %  
+    SERVER3         Server 2008 SP2    System Managed   4394         4094           0.0000 %    79.998         10.027         12.5300 %  
+    SERVER4         Server 2012        c:\pagefile.sys  6144         8191           75.0000 %   59.655         35.909         60.1900 %  
 
-Get-Content "C:\Scripts\Netbackup\Computers.txt" | % {
+    .Outputs
+    One or more PSObjects with info about Page File Size, RAM, and Hard Disk as well as an "Export.csv" will be built. 
+#>
+
+function Get-PageFile_and_Disk_Report {
+$Results = @()
+Get-Content $args[0] | % {
     $Server = $_
         $Server2k12 = $_
         $Server2k8R2 = $_
@@ -22,7 +41,7 @@ Get-Content "C:\Scripts\Netbackup\Computers.txt" | % {
     $SizeOnC = [Math]::Round($disk.Size/1GB,3)
     $FreeOnC = [Math]::Round($disk.FreeSpace/1GB,3)
     $PercentFreeOnC = "{0:P4}" -f [Math]::Round([Math]::Round($disk.FreeSpace/1GB,3)/[Math]::Round($disk.Size/1GB,3),4)
-
+    
 ForEach($Item in $Server){
 If($OSVersion -eq "6.2.9200"){
     Foreach ($Item in $Server2k12){
@@ -38,6 +57,7 @@ If($OSVersion -eq "6.2.9200"){
                 $TempOBJ | Add-Member -MemberType NoteProperty -Name "Size of C - GB" -Value $SizeOnC
                 $TempOBJ | Add-Member -MemberType NoteProperty -Name "Free on C - GB" -Value $FreeOnC
                 $TempOBJ | Add-Member -MemberType NoteProperty -Name "% Free on C" -Value $PercentFreeOnC
+
                 }
             Else
                 {
@@ -178,3 +198,4 @@ ElseIf($OSVersion -eq "5.2.3790"){
 }
 $results | Export-Csv C:\Scripts\netbackup\Export.csv -Force -NoTypeInformation
 Import-CSV C:\Scripts\netbackup\Export.csv | Format-Table -Autosize
+}
