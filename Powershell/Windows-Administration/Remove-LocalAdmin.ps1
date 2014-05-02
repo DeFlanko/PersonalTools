80d874fd14a7709e53b2213fe8e83a4b449ec649
DeFlanko <DeFlanko@users.noreply.github.com>
2014-05-02 12:09:36 -0700

    Update Remove-LocalAdmin.ps1
    Multi-InputBox // Single user input multi server input

--- Powershell/Windows-Administration/Remove-LocalAdmin.ps1
+++ Powershell/Windows-Administration/Remove-LocalAdmin.ps1
@@ -1,49 +1,162 @@
-function ListAdministrators($Group)
-{
-$members= $Group.psbase.invoke("Members") | %{$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}
-$members
+# Prerequisite Functions\
+function ListAdministrators($Group){
+    $members= $Group.psbase.invoke("Members") | %{$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}
+    $members
 }
 function Ping-Server {
-Param([string]$srv)
-$pingresult = Get-WmiObject Win32_PingStatus -Filter "Address='$srv'"
-if($pingresult.StatusCode -eq 0) {$true} else {$false}
+    Param([string]$srv)
+    $pingresult = Get-WmiObject Win32_PingStatus -Filter "Address='$srv'"
+    if($pingresult.StatusCode -eq 0) {$true} else {$false}
 }
-#sets ARGS.. if you want to manually specify a list other than ".\REMOVESERVERS.txt" then change ARGS to 2 and specify at the end...
-if ($args.Length -ne 1) {
-Write-host "================================================="
-Write-Host "`tUsage: "
-Write-Host -foregroundcolor Green "`t`t.\RemoveLocalAdmin.ps1" "(UserName) or (Security Group)"
-Write-Host "`tExample(s):"
-Write-Host -foregroundcolor Cyan "`t`t .\RemoveLocalAdmin.ps1" "(UserName) or (Security Group)" 
-Write-Host -foregroundcolor Cyan "`t`t C:\Scripts\Tools\PS1\LocalAdmin\RemoveLocalAdmin.ps1" "(UserName) or (Security Group)"
-Write-host "================================================="
-return
-}
-#Your domain, change this
-$domain = "MMC"
-#Get the user to add
-$username = $args[0]
-#File to read computer list from
-$strComputers = Get-content "C:\Scripts\Tools\PS1\LocalAdmin\REMOVESERVERS.txt"
-foreach ($strComputer in $strComputers)
-{ 
+
+#Remove-LocalAdmin
+function Remove_LocalAdmin ($strComputer){ 
 if (Ping-Server($strComputer)) { 
-$computer = [ADSI]("WinNT://" + $strComputer + ",computer")
-$GroupName = "Administrators"
-$Group = $computer.psbase.children.find($Groupname)
+    $computer = [ADSI]("WinNT://" + $strComputer + ",computer")
+    $GroupName = "Administrators"
+    $Group = $computer.psbase.children.find($Groupname)
 # This will list what’s currently in Administrator Group so you can verify the result
-write-host -foregroundcolor green "====== $strComputer $Groupname BEFORE ====="
-ListAdministrators $Group
-write-host -foregroundcolor green "====== BEFORE ====="
+    write-host -foregroundcolor green "====== $strComputer $Groupname BEFORE ====="
+    ListAdministrators $Group
+    write-host -foregroundcolor green "====== BEFORE ====="
 # Even though we are adding the AD account
-# It is being added to the local computer and so we will need to use WinNT: provider 
-$Group.remove("WinNT://" + $domain + "/" + $username) 
-write-host -foregroundcolor green "====== $strComputer $Groupname AFTER ====="
-ListAdministrators $Group
-write-host -foregroundcolor green "====== AFTER ====="
-}
+# It is being added to the local computer and so we will need to use WinNT: provider
+###### Besure to modify your Domain here ##### 
+    $Group.remove("WinNT://" + "MMC" + "/" + $ItemUser) 
+    write-host -foregroundcolor green "====== $strComputer $Groupname AFTER ====="
+    ListAdministrators $Group
+    write-host -foregroundcolor green "====== AFTER ====="
+    }
 else
+    {
+        write-host -foregroundcolor red "$strComputer is not pingable"
+    }
+}
+
+## Lets Build an InputBox
+function GetInputUser ($DefaultText = "",$LabelMessage = "Please enter the information in the space below:",$MultiLine = $true)
 {
-write-host -foregroundcolor red "$strComputer is not pingable"
-}
-}
+    $objForm = New-Object System.Windows.Forms.Form 
+    $objForm.Text = "User to Remove Input Box"
+    $objForm.Size = New-Object System.Drawing.Size(300,200) 
+    $objForm.StartPosition = "CenterScreen"
+
+    $objForm.KeyPreview = $True
+    If(!$MultiLine)
+    {
+        $objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") {$objForm.Close()}})
+    }
+    $objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") {$objForm.Close()}})
+
+    $OKButton = New-Object System.Windows.Forms.Button
+    $OKButton.Location = New-Object System.Drawing.Size(75,125)
+    $OKButton.Size = New-Object System.Drawing.Size(75,23)
+    $OKButton.Text = "OK"
+    $OKButton.Add_Click({$objForm.Close()})
+    $objForm.Controls.Add($OKButton)
+
+    $CancelButton = New-Object System.Windows.Forms.Button
+    $CancelButton.Location = New-Object System.Drawing.Size(150,125)
+    $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+    $CancelButton.Text = "Cancel"
+    $CancelButton.Add_Click({$objForm.Close()})
+    $objForm.Controls.Add($CancelButton)
+
+    $objLabel = New-Object System.Windows.Forms.Label
+    $objLabel.Location = New-Object System.Drawing.Size(10,10) 
+    $objLabel.Size = New-Object System.Drawing.Size(280,20) 
+    $objLabel.Text = $LabelMessage
+    $objForm.Controls.Add($objLabel) 
+
+    $objTextBox = New-Object System.Windows.Forms.TextBox 
+    $objTextBox.Location = New-Object System.Drawing.Size(10,30) 
+    $objTextBox.Size = New-Object System.Drawing.Size(260,85)
+    If($MultiLine)
+    {
+        $objTextBox.multiline = $true
+        #$objTextBox.ScrollBars = $True
+    }
+    If($DefaultText.length -gt 0)
+    {
+        $objTextBox.Text = $DefaultText
+    }
+
+    $objForm.Controls.Add($objTextBox) 
+    $objForm.Topmost = $True
+    $objForm.Add_Shown({$objForm.Activate()})
+    [void] $objForm.ShowDialog()
+
+    $objTextBox.Text
+} 
+
+function GetInputServer ($DefaultText = "",$LabelMessage = "Please enter the information in the space below:",$MultiLine = $true)
+{
+    $objForm = New-Object System.Windows.Forms.Form 
+    $objForm.Text = "Server List Input Box"
+    $objForm.Size = New-Object System.Drawing.Size(300,200) 
+    $objForm.StartPosition = "CenterScreen"
+
+    $objForm.KeyPreview = $True
+    If(!$MultiLine)
+    {
+        $objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") {$objForm.Close()}})
+    }
+    $objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") {$objForm.Close()}})
+
+    $OKButton = New-Object System.Windows.Forms.Button
+    $OKButton.Location = New-Object System.Drawing.Size(75,125)
+    $OKButton.Size = New-Object System.Drawing.Size(75,23)
+    $OKButton.Text = "OK"
+    $OKButton.Add_Click({$objForm.Close()})
+    $objForm.Controls.Add($OKButton)
+
+    $CancelButton = New-Object System.Windows.Forms.Button
+    $CancelButton.Location = New-Object System.Drawing.Size(150,125)
+    $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+    $CancelButton.Text = "Cancel"
+    $CancelButton.Add_Click({$objForm.Close()})
+    $objForm.Controls.Add($CancelButton)
+
+    $objLabel = New-Object System.Windows.Forms.Label
+    $objLabel.Location = New-Object System.Drawing.Size(10,10) 
+    $objLabel.Size = New-Object System.Drawing.Size(280,20) 
+    $objLabel.Text = $LabelMessage
+    $objForm.Controls.Add($objLabel) 
+
+    $objTextBox = New-Object System.Windows.Forms.TextBox 
+    $objTextBox.Location = New-Object System.Drawing.Size(10,30) 
+    $objTextBox.Size = New-Object System.Drawing.Size(260,85)
+    If($MultiLine)
+    {
+        $objTextBox.multiline = $true
+        #$objTextBox.ScrollBars = $True
+    }
+    If($DefaultText.length -gt 0)
+    {
+        $objTextBox.Text = $DefaultText
+    }
+
+    $objForm.Controls.Add($objTextBox) 
+    $objForm.Topmost = $True
+    $objForm.Add_Shown({$objForm.Activate()})
+    [void] $objForm.ShowDialog()
+
+    $objTextBox.Text
+} 
+
+$ItemUser = GetInputUser -LabelMessage "Input a User to Remove from Local Admins:" -MultiLine $false
+$ItemList = GetInputServer -LabelMessage "Input FQDN of Servers to Process:" -MultiLine $true
+
+$ItemList = $ItemList.Split()
+
+foreach ($Item in $ItemList)
+{
+    if (Test-Connection $Item -Count 1) 
+    {
+        Remove_LocalAdmin -strComputer $Item 
+    }
+    Else
+    {
+        Write-Host -ForegroundColor Red "Cannot Connect to $Item"
+    }
+} 
