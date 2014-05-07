6f41889def7cf36f79a2a08ff66c8f06d7d85c68
DeFlanko <DeFlanko@users.noreply.github.com>
2014-05-07 14:29:56 -0700

    Update and rename Get-LocalAdmin.ps1 to Get-List_Of_User_In_Group.ps1

--- Powershell/Windows-Administration/Get-List_Of_User_In_Group.ps1
+++ Powershell/Windows-Administration/Get-List_Of_User_In_Group.ps1
@@ -1,24 +1,97 @@
 ## Prerequisite Functions
-function ListAdministrators($Group){
+Function ListMembers($Group){
     $members= $Group.psbase.invoke("Members") | %{$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}
     $members
     }
 
-function Ping-Server{
+Function Ping-Server{
     Param([string]$srv)
     $pingresult = Get-WmiObject Win32_PingStatus -Filter "Address='$srv'"
     if($pingresult.StatusCode -eq 0) {$true} else {$false}
     }
 
-## Get-LocalAdmins
+Function SelectGroup{
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+$objForm = New-Object System.Windows.Forms.Form 
+$objForm.Text = "Select a Group"
+$objForm.Size = New-Object System.Drawing.Size(300,200) 
+$objForm.StartPosition = "CenterScreen"
+
+# Got rid of the block of code related to KeyPreview and KeyDown events.
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+
+# Got rid of the Click event for the OK button, and instead just assigned its DialogResult property to OK.
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+
+$objForm.Controls.Add($OKButton)
+
+# Setting the form's AcceptButton property causes it to automatically intercept the Enter keystroke and
+# treat it as clicking the OK button (without having to write your own KeyDown events).
+$objForm.AcceptButton = $OKButton
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Size(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+
+# Got rid of the Click event for the Cancel button, and instead just assigned its DialogResult property to Cancel.
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+
+$objForm.Controls.Add($CancelButton)
+
+# Setting the form's CancelButton property causes it to automatically intercept the Escape keystroke and
+# treat it as clicking the OK button (without having to write your own KeyDown events).
+$objForm.CancelButton = $CancelButton
+
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(10,20) 
+$objLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objLabel.Text = "Please select a Group:"
+$objForm.Controls.Add($objLabel) 
+
+$objListBox = New-Object System.Windows.Forms.ListBox 
+$objListBox.Location = New-Object System.Drawing.Size(10,40) 
+$objListBox.Size = New-Object System.Drawing.Size(260,20) 
+$objListBox.Height = 80
+
+[void] $objListBox.Items.Add("Administrators")
+[void] $objListBox.Items.Add("Remote Desktop Users")
+[void] $objListBox.Items.Add("Power Users")
+[void] $objListBox.Items.Add("User")
+
+$objForm.Controls.Add($objListBox) 
+
+$objForm.Topmost = $True
+
+# Now, instead of having events in the form assign a value to a variable outside of their scope, the code that calls the dialog
+# instead checks to see if the user pressed OK and selected something from the box, then grabs that value.
+
+$result = $objForm.ShowDialog()
+if ($result -eq [System.Windows.Forms.DialogResult]::OK -and $objListBox.SelectedIndex -ge 0)
+{
+    $SelectedGroup = $objListBox.SelectedItem
+    # Do something with $SelectedGroup
+    $SelectedGroup
+}
+}
+
+
+
+## Get-UsersOfGroup
 function Get_LocalAdmin ($strComputer){
 if (Ping-Server($strComputer))
     { 
     $computer = [ADSI]("WinNT://" + $strComputer + ",computer")
-    $GroupName = "Administrators"
+    $GroupName = [string](SelectGroup)
     $Group = $computer.psbase.children.find($Groupname)
     write-host -foregroundcolor green "====== $strComputer $Groupname List ====="
-    ListAdministrators $Group
+    ListMembers $Group
     write-host -foregroundcolor green "====== END ====="
     }
 else
