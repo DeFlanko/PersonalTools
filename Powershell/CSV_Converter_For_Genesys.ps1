814688ec710607460f18e9d1196471416669aaae
DeFlanko <DeFlanko@users.noreply.github.com>
2022-03-16 16:43:25 -0700

    Add files via upload

---
+++ Powershell/CSV_Converter_For_Genesys.ps1
@@ -0,0 +1,4 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
+$null = $FileBrowser.ShowDialog()
+Get-Content $FileBrowser.FileName | Set-Content -Encoding string $FileBrowser.FileName.Replace('.csv','_Converted.csv')
